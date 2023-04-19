729dbc468b2c6d179f486e35dfae7dd6b67126a3
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-19 16:39:58 +0200

    Add file compression with ghostscript

--- export-example.sh
+++ export-example.sh
@@ -5,3 +5,12 @@ set -o errexit
 cd src
 yarn && yarn export-example
 cd ..
+
+if ! command -v gs &> /dev/null
+then
+    echo "skipping file compression with ghostscript"
+    exit
+fi
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/slides.example-compressed.pdf docs/slides.example.pdf
+mv docs/slides.example-compressed.pdf docs/slides.example.pdf
--- export.sh
+++ export.sh
@@ -5,3 +5,15 @@ set -o errexit
 cd src
 yarn && yarn export
 cd ..
+
+if ! command -v gs &> /dev/null
+then
+    echo "skipping file compression with ghostscript"
+    exit
+fi
+
+# compresing `slides.pdf` and `slides-with-clicks.pdf` with ghostscript
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=slides-compressed.pdf slides.pdf
+mv slides-compressed.pdf slides.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=slides-with-clicks-compressed.pdf slides-with-clicks.pdf
+mv slides-with-clicks-compressed.pdf slides-with-clicks.pdf
